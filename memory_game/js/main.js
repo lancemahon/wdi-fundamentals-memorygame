68d8c12d7275c11612cc3a9d625eabe4450271eb
DESKTOP-9RHET8R\lance <lance.mahon@gmail.com>
2019-07-26 12:38:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -14,8 +36,11 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId] + ".");
-	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 }
