d54a45fe44ece659fc140ce1218a0656fb097ed3
DESKTOP-9RHET8R\lance <lance.mahon@gmail.com>
2019-07-26 11:38:58 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,23 @@ console.log("Up and running!");
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cardId);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
